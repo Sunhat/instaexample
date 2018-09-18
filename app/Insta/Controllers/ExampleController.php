4e7156b4824c47ab8fce9740d9bc321747d7d483
Marcus Coleman <marcus.coleman@bauermedia.co.uk>
2018-09-18 19:03:40 +0100

    Remove whitespace

--- app/Insta/Controllers/ExampleController.php
+++ app/Insta/Controllers/ExampleController.php
@@ -2,10 +2,8 @@
 namespace Insta\Controllers;
 
 class ExampleController extends Controller {
-	
 	public function index($request, $response)
 	{
 		return $this->response($request, $response, 'example');
 	}
-
 }
